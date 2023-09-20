e642eab3a662a717d2c80414c941fe0325ef5a34
Juanceros <62163590+Zenis-Shrestha@users.noreply.github.com>
2023-09-20 22:42:28 +0545

    add-login

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require ('express');
 const bcrypt = require('bcrypt');
 const { users } = require('./Model/index');
+const e = require('express');
 const app  = express()
 app.set("view engine","ejs")
 
@@ -30,8 +31,46 @@ app.post("/register",async (req,res)=>{
         username:username,
         password :bcrypt.hashSync(password,10),
     })
-    res.send("User Registeration Successful");
+ res.redirect("/login")
 })
+
+
+//user login part
+app.get("/login",(req,res)=>{
+    res.render("login")
+})
+
+app.post("/login",async(req,res)=>{
+ const email = req.body.email
+ const password = req.body.password
+
+ //check user entered email with email in database
+
+    const userExists = await users.findAll({
+        where:{
+            email:email
+        }
+    })
+    // console.log(userExists)
+
+
+ if(userExists.length> 0){
+    //compare user entered password with passwword in database
+     const isMatch = bcrypt.compareSync(password,userExists[0].password)
+     if(isMatch){
+        res.send("Logged In Successfully")
+     }else{
+        res.send("Invalid Email or Password")
+     }
+ }
+ else{
+    res.send('Invalid Email and Password')
+ }
+})
+
+
+
+
 app.listen(3000,function(){
     console.log('server is running on port: ',3000)
 });  
